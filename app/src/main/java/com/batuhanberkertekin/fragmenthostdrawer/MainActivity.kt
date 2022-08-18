f7c20d119bc7117484eece923129613ecff86851
Batuhan Berk Ertekin <batuhandumpas@gmail.com>
2022-08-18 18:45:33 +0300

    hello world yazıldı

---
+++ app/src/main/java/com/batuhanberkertekin/fragmenthostdrawer/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.batuhanberkertekin.fragmenthostdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.baslik.view.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navhost =supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+NavigationUI.setupWithNavController(NavigationView,navhost.navController)
+
+        toolbar.title = "başlık"
+
+        val toogle =ActionBarDrawerToggle(this,DrawerLayout,toolbar,0,0)
+
+        DrawerLayout.addDrawerListener(toogle)
+toogle.syncState()
+
+        val baslik = NavigationView.inflateHeaderView(R.layout.baslik)
+        baslik.baslikText.text = "Hello World "
+
+
+
+
+    }
+
+    override fun onBackPressed() {
+
+        if(DrawerLayout.isDrawerOpen(GravityCompat.START)){
+           DrawerLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+
+
+    }
+}
